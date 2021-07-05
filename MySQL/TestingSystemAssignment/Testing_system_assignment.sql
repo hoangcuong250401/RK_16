7bc40eaa2b4e7ee5c98c3ca7d793aefcf7ef7fc4
hoangcuong250401 <86681246+hoangcuong250401@users.noreply.github.com>
2021-07-05 19:40:11 +0700

    Update Testing_system_assignment.sql

--- MySQL/TestingSystemAssignment/Testing_system_assignment.sql
+++ MySQL/TestingSystemAssignment/Testing_system_assignment.sql
@@ -2,78 +2,112 @@ DROP DATABASE IF EXISTS `Testing_System_Assignment_1`;
 
 CREATE DATABASE `Testing_System_Assignment_1`;
 USE `Testing_System_Assignment_1`;
-DROP TABLE IF EXISTS `Department`;
-CREATE TABLE `Department`(
-	Department_ID 	TINYINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    Department_Name VARCHAR(50)
+-- create table 1: Department
+DROP TABLE IF EXISTS Department;
+CREATE TABLE Department(
+	DepartmentID 			TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    DepartmentName 			NVARCHAR(30) NOT NULL UNIQUE KEY
 );
+
+-- create table 2: Posittion
 DROP TABLE IF EXISTS Position;
-CREATE TABLE Position (
-	Position_ID 	SMALLINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    Position_Name	VARCHAR(50)
+CREATE TABLE `Position` (
+	PositionID				TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    PositionName			ENUM('Dev','Test','Scrum Master','PM') NOT NULL UNIQUE KEY
 );
+
+-- create table 3: Account
 DROP TABLE IF EXISTS `Account`;
 CREATE TABLE `Account`(
-	Account_ID 		SMALLINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    Email			VARCHAR(30) UNIQUE KEY,
-    Username		CHAR(15) UNIQUE KEY,
-    FullName		NVARCHAR(50),
-    Department_ID	TINYINT UNSIGNED,
-    Position_ID		SMALLINT UNSIGNED,
-    Create_Date		DATETIME
+	AccountID				TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    Email					VARCHAR(50) NOT NULL UNIQUE KEY,
+    Username				VARCHAR(50) NOT NULL UNIQUE KEY,
+    FullName				NVARCHAR(50) NOT NULL,
+    DepartmentID 			TINYINT UNSIGNED NOT NULL,
+    PositionID				TINYINT UNSIGNED NOT NULL,
+    CreateDate				DATETIME DEFAULT NOW(),
+    FOREIGN KEY(DepartmentID) REFERENCES Department(DepartmentID),
+    FOREIGN KEY(PositionID) REFERENCES `Position`(PositionID)
 );
+
+-- create table 4: Group
 DROP TABLE IF EXISTS `Group`;
-CREATE TABLE `Group` (
-	Group_ID		INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    Group_Name		VARCHAR(30),
-    Creator_ID		SMALLINT,
-    Create_Date		DATETIME
+CREATE TABLE `Group`(
+	GroupID					TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    GroupName				NVARCHAR(50) NOT NULL UNIQUE KEY,
+    CreatorID				TINYINT UNSIGNED NOT NULL,
+    CreateDate				DATETIME DEFAULT NOW()
 );
-DROP TABLE IF EXISTS `GroupAccount`;
-CREATE TABLE `GroupAccount`(
-	Group_ID		INT,
-    Account_ID		SMALLINT,
-    Join_Date		DATE
+
+-- create table 5: GroupAccount
+DROP TABLE IF EXISTS GroupAccount;
+CREATE TABLE GroupAccount(
+	GroupID					TINYINT UNSIGNED NOT NULL,
+    AccountID				TINYINT UNSIGNED NOT NULL,
+    JoinDate				DATETIME DEFAULT NOW(),
+    PRIMARY KEY (GroupID,AccountID)
 );
-DROP TABLE IF EXISTS Type_Question;
-CREATE TABLE Type_Question (
-	Type_ID			SMALLINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    Type_Name		VARCHAR(20)
+
+-- create table 6: TypeQuestion
+DROP TABLE IF EXISTS TypeQuestion;
+CREATE TABLE TypeQuestion (
+    TypeID 			TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    TypeName 		ENUM('Esay','Multiple-Choice') NOT NULL UNIQUE KEY
 );
-DROP TABLE IF EXISTS Category_Question;
-CREATE TABLE Category_Question(
-	Category_ID		TINYINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    Category_Name	CHAR(20)
+
+-- create table 7: CategoryQuestion
+DROP TABLE IF EXISTS CategoryQuestion;
+CREATE TABLE CategoryQuestion(
+    CategoryID				TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    CategoryName			NVARCHAR(50) NOT NULL UNIQUE KEY
 );
+
+-- create table 8: Question
 DROP TABLE IF EXISTS Question;
 CREATE TABLE Question(
-	Question_ID		TINYINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    Content			VARCHAR(100),
-    Category_ID		SMALLINT,
-    Type_ID			SMALLINT,
-    Creator_ID		SMALLINT,
-    Create_Date		DATETIME
+    QuestionID				TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    Content					NVARCHAR(100) NOT NULL,
+    CategoryID				TINYINT UNSIGNED NOT NULL,
+    TypeID					TINYINT UNSIGNED NOT NULL,
+    CreatorID				TINYINT UNSIGNED NOT NULL UNIQUE KEY,
+    CreateDate				DATETIME DEFAULT NOW(),
+    FOREIGN KEY(CategoryID) 	REFERENCES CategoryQuestion(CategoryID),
+    FOREIGN KEY(TypeID) 		REFERENCES TypeQuestion(TypeID),
+    FOREIGN KEY(CreatorID) 		REFERENCES `Account`(AccountId)
 );
+
+-- create table 9: Answer
 DROP TABLE IF EXISTS Answer;
 CREATE TABLE Answer(
-	Answer_ID		SMALLINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    Content			VARCHAR(100),
-    Question_ID		TINYINT,
-	IsCorrect		BOOLEAN
+    Answers					TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    Content					NVARCHAR(100) NOT NULL,
+    QuestionID				TINYINT UNSIGNED NOT NULL,
+    isCorrect				BIT DEFAULT 1,
+    FOREIGN KEY(QuestionID) REFERENCES Question(QuestionID)
 );
-    
+
+-- create table 10: Exam
+DROP TABLE IF EXISTS Exam;
 CREATE TABLE Exam(
-	Exam_ID			SMALLINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    `Code`			TINYINT,
-    Title			NVARCHAR(50),
-    Caregory_ID		TINYINT UNSIGNED,
-    Duration		TIME,
-    Creator_ID		SMALLINT UNSIGNED,
-    Create_Date		DATETIME
+    ExamID					TINYINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    `Code`					CHAR(10) NOT NULL,
+    Title					NVARCHAR(50) NOT NULL,
+    CategoryID				TINYINT UNSIGNED NOT NULL,
+    Duration				TINYINT UNSIGNED NOT NULL,
+    CreatorID				TINYINT UNSIGNED NOT NULL,
+    CreateDate				DATETIME DEFAULT NOW(),
+    FOREIGN KEY(CategoryID) REFERENCES CategoryQuestion(CategoryID),
+    FOREIGN KEY(CreatorID) 	REFERENCES `Account`(AccountId)
 );
-CREATE TABLE Exam_Question(
-	Exam_ID			SMALLINT UNSIGNED,
-    Question_ID		TINYINT UNSIGNED
+
+-- create table 11: ExamQuestion
+DROP TABLE IF EXISTS ExamQuestion;
+CREATE TABLE ExamQuestion(
+    ExamID				TINYINT UNSIGNED NOT NULL,
+	QuestionID			TINYINT UNSIGNED NOT NULL,
+    FOREIGN KEY(QuestionID) REFERENCES Question(QuestionID),
+    FOREIGN KEY(ExamID) REFERENCES Exam(ExamID),
+    PRIMARY KEY (ExamID,QuestionID)
 );
 	
 
